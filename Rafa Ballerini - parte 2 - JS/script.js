3999dd3d46bc9b8cd56f29361b18f388ef319bf2
Heloisa Duarte <heloisaa.sdp@gmail.com>
2025-07-07 12:09:21 -0300

    Atualização

--- Rafa Ballerini - parte 2 - JS/script.js
+++ Rafa Ballerini - parte 2 - JS/script.js
@@ -42,9 +42,9 @@ function renderizarTarefas() {
       //i++ = i+1
 
 
-      let i = 0;
+     
 
-      for (i; i < tarefas.length; i++) {
+      for ( let i = 0; i < tarefas.length; i++) {
             let novaTarefa = document.createElement("li");
             novaTarefa.textContent = tarefas[i];
 
@@ -53,9 +53,46 @@ function renderizarTarefas() {
             botaoRemover.className = "remover";
             botaoRemover.textContent = "Remover";
 
+            botaoRemover.onclick = function() {
+                  removerTarefa(i);
+      }
+
+
+
+            let botaoEditar = document.createElement("button");
+            botaoEditar.className = "editar";
+            botaoEditar.textContent = "Editar";
+            
+            botaoEditar.onclick = function() {
+                  editarTarefa(i);
+            }
+
+
             novaTarefa.appendChild(botaoRemover);
+            novaTarefa.appendChild(botaoEditar);
             listaTarefas.appendChild(novaTarefa);
+      }
+
+      function removerTarefa(i) {
+            tarefas.splice(i, 1);
+            renderizarTarefas();
+      }
+      
+      function editarTarefa(i) {
+            let tarefaEditada = prompt("Editar tarefa")
+
+            if (tarefaEditada.trim !== "") {
+                  tarefas[i] = tarefaEditada;
+                  renderizarTarefas();
+            }
       }
 
       
 }
+
+function limparLista() {
+            tarefas.length = 0;
+            renderizarTarefas();
+            const mensagem = document.getElementById("mensagem");
+            mensagem.textContent = "Lista limpa com sucesso!";
+      }
